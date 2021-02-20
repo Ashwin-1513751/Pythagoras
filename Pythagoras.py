fcc8cb112057e82e48622a7c83cb262062d6fc7e
Ash <darkovl10@gmail.com>
2021-02-20 11:52:16 +0000

    Create Pythagoras.py

---
+++ Pythagoras.py
@@ -0,0 +1,15 @@
+x = input("What is the value of the first side?")
+
+y = input("What is the value of the second side?")
+
+x = float(x)
+y = float(y)
+
+import math
+
+z = x**2 + y**2
+
+z = math.sqrt(z)
+print(z)
+
+print("This is the answer...")
